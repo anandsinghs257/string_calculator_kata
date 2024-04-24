093a8e5d76c47035b5611ca5d7af23433b49067b
anandsinghs257 <anandsinghs257@gmail.com>
2024-04-24 22:13:45 +0530

    method will handle new lines between numbers instead of commas

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,13 +1,26 @@
-
-
 def add(numbers)
   return 0 if numbers.empty?
 
-  numbers.gsub('"', '').split(',').map(&:to_i).sum
+  # Remove surrounding quotes if present
+  numbers = numbers[1..-2] if numbers.start_with?('"') && numbers.end_with?('"')
+
+  # Check for invalid input: comma followed by newline within quotes
+  if numbers.include?('\n,') || numbers.include?(',\n')
+    raise ArgumentError, "Invalid input: #{numbers}"
+  end
+
+  # Replace occurrences of "\n" within quotes with ","
+  numbers.gsub!('\n', ',') if numbers.include?('\n')
+
+  # Split by comma and sum
+  numbers.split(",").map(&:to_i).sum
 end
 
-
-print "Enter numbers separated by commas: "
+print "Enter numbers separated by commas or new lines: "
 input = gets.chomp
 
-puts add(input)
+begin
+  puts add(input)
+rescue ArgumentError => e
+  puts e.message
+end
